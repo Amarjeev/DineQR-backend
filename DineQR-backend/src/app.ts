21ff0f9a256660533abf285296598fda95e8e32e
Amarjeev Ms <amarjeevm@gmail.com>
2025-09-07 18:48:59 +0530

    Database config done

--- DineQR-backend/src/app.ts
+++ DineQR-backend/src/app.ts
@@ -1,14 +1,29 @@
-import express, { Application} from "express";
+import  dotenv  from 'dotenv';
+import express, { Application } from "express";
+import connectDB from "./config/database"; 
+
+dotenv.config();
 
 const app: Application = express();
-const PORT: number = 5000;
+const PORT: number = Number(process.env.PORT) || 5000;
 
 // Middleware (optional)
 app.use(express.json());
+;
 
+// Function to start server after DB connection
+const startServer = async () => {
+  try {
+    await connectDB();
 
+    app.listen(PORT, () => {
+      console.log(`🚀 Server running on http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error("❌ Failed to connect to MongoDB:", error);
+    process.exit(1);
+  }
+};
 
-// Start server
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
-});
+// Call startServer
+startServer();
