d8324fd79079e9ab4033d82207c5939864fac772
Amarjeev Ms <amarjeevm@gmail.com>
2025-09-10 20:30:41 +0530

    cors url added

--- DineQR-backend/src/app.ts
+++ DineQR-backend/src/app.ts
@@ -33,7 +33,7 @@ const PORT: number = Number(process.env.PORT) || 5000;
 
 // ✅ CORS options
 const corsOptions: CorsOptions = {
-  origin: ["http://localhost:5173", "https://dineqr-website.vercel.app"], // allowed frontends
+  origin: ["http://localhost:5173", "https://dine-qr-website.vercel.app/"], // allowed frontends
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // allowed HTTP methods
   credentials: true, // allow cookies or auth headers
 };
