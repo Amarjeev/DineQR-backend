c10996908599161198a740d671392fbee486d688
Amarjeev Ms <amarjeevm@gmail.com>
2025-09-10 20:39:22 +0530

    allowedHeaders ADDED

--- DineQR-backend/src/app.ts
+++ DineQR-backend/src/app.ts
@@ -34,7 +34,8 @@ const PORT: number = Number(process.env.PORT) || 5000;
 // ✅ CORS options
 const corsOptions: CorsOptions = {
   origin: ["http://localhost:5173", "https://dine-qr-website.vercel.app/"], // allowed frontends
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // allowed HTTP methods
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // allowed HTTP methods
+  allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true, // allow cookies or auth headers
 };
 
@@ -47,8 +48,6 @@ app.use(express.json()); // Parse JSON request bodies
 
 // Apply CORS middleware
 app.use(cors(corsOptions));
-
-
 
 /**
  * --------------------------
