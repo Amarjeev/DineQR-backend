733df2bb8b6a13f9eb9fe4ff5eddaa79c2d75545
Amarjeev Ms <amarjeevm@gmail.com>
2025-09-11 22:31:26 +0530

    few changes

--- DineQR-backend/src/types.d.ts
+++
@@ -1,8 +0,0 @@
-// import 'express';
-
-// declare module 'express' {
-//   interface Request {
-//     cookies?: { [key: string]: string };
-//     user?: any; // for your AuthRequest user payload
-//   }
-// }
